2b943c718fb91d9282f2f2955c5fbca8fac90725
cyyyu <cyu9960@gmail.com>
2018-01-15 14:41:40 +0800

    list style

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,63 @@
 import * as React from 'react'
 import { observer } from 'mobx-react'
+import { css } from 'glamor'
+
+const style_List = css({
+  margin: '16px auto',
+  width: '85%',
+  maxWidth: 350,
+  height: 'calc(100vh - 180px)',
+  overflow: 'auto',
+  borderRadius: 4,
+  padding: '6px 12px',
+  '& ul': {
+    display: 'block',
+    padding: 0,
+    margin: 0,
+    '& li': {
+      display: 'flex',
+      alignItems: 'center',
+      listStyle: 'none',
+      padding: '10px 12px',
+      borderRadius: 4,
+      background: 'white',
+      boxShadow: '0 0 4px gray',
+      '&:not(:last-child)': {
+        marginBottom: 6
+      },
+      '& .item-index': {
+        fontSize: 12,
+        marginRight: 6,
+        '&:after': {
+          content: '.'
+        }
+      },
+      '& button.item-remove': {
+        marginLeft: 'auto',
+        fontSize: 18,
+        textShadow: '0 0 4px #a9a9a9'
+      }
+    }
+  }
+})
 
 const List = observer(props => (
-  <ul>{props.store.list.map(item => <li key={item.id}><button onClick={() => props.store.removeItem(item.id)}>remove</button>{item.text}</li>)}</ul>
+  <div {...style_List}>
+    <ul>
+      {props.store.list.map((item, index) => (
+        <li key={item.id}>
+          <span className="item-index">{index + 1}</span>
+          {item.text}
+          <button
+            className="item-remove"
+            onClick={() => props.store.removeItem(item.id)}
+          >
+            &#x2716;
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
 ))
 
 export default List
