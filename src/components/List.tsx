5635995dd181f9616602cc224ca134b4bc7ed021
cyyyu <cyu9960@gmail.com>
2018-01-16 15:49:34 +0800

    animate out

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react'
 import { observer } from 'mobx-react'
-import { css } from 'glamor'
+import { css, keyframes } from 'glamor'
+import { AppStore } from '../store'
+import * as classnames from 'classnames'
+
+const fadeOut = keyframes('fadeOut', {
+  from: {
+    transform: 'scale(1)',
+    opacity: 1
+  },
+  to: {
+    transform: 'scale(0.8)',
+    opacity: 0
+  }
+})
 
 const style_List = css({
   margin: '16px auto',
@@ -22,6 +35,9 @@ const style_List = css({
       borderRadius: 4,
       background: 'white',
       boxShadow: '0 0 4px gray',
+      '&.fade-out': {
+        animation: `${fadeOut} .15s forwards`,
+      },
       '&:not(:last-child)': {
         marginBottom: 6
       },
@@ -41,23 +57,51 @@ const style_List = css({
   }
 })
 
-const List = observer(props => (
-  <div {...style_List}>
-    <ul>
-      {props.store.list.map((item, index) => (
-        <li key={item.id}>
-          <span className="item-index">{index + 1}</span>
-          {item.text}
-          <button
-            className="item-remove"
-            onClick={() => props.store.removeItem(item.id)}
-          >
-            &#x2716;
-          </button>
-        </li>
-      ))}
-    </ul>
-  </div>
-))
+@observer
+class List extends React.Component<{ store: AppStore }, {
+  animatings: {
+    [id: string]: boolean
+  }
+}> {
+  state = {
+    animatings: {}
+  }
+
+  addForAnimatingOut = (id) => () => {
+    const { store } = this.props
+    this.setState((state) => {
+      const animatings = { ...state.animatings }
+      animatings[id] = true
+      return { animatings }
+    }, () => {
+      // remove from db by the end of animation
+      setTimeout(() => {
+        store.removeItem(id)
+      }, 200)
+    })
+  }
+
+  render() {
+    const { store } = this.props
+    return (
+      <div {...style_List}>
+        <ul>
+          {store.list.map((item, index) => (
+            <li key={item.id} className={classnames({ 'fade-out': !!this.state.animatings[item.id] })}>
+              <span className="item-index">{index + 1}</span>
+              {item.text}
+              <button
+                className="item-remove"
+                onClick={this.addForAnimatingOut(item.id)}
+              >
+                &#x2716;
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default List
